17d42030822c72a42bb9a968f9f24a963a555d75
Gary Wright <gary.r.wright@mac.com>
2013-06-01 13:14:00 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cthackday::Application.config.secret_token = 'a961aae2e32c95a3094c9c5e5a7f22e2780c3b5e607218d4b628e213b541c8982ed85e4e27fe91cfeb6593e9644d43831d8943473a17940f589a734af5a65cfd'
